d6fef76d75a72106a5c6e1019f2ab6d281d7a2da
QianjunL <qianjunliang.ql@gmail.com>
2024-09-11 11:37:58 -0600

    complete integration of backend to display property

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,14 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 
+export type PropertyType = {
+  id: string;
+  title: string;
+  image_url: string;
+  price_per_night: number;
+};
+
 const PropertyList = () => {
+  const [propertyList, setPropertyList] = useState<PropertyType[]>([]);
+
+  const getPropertyList = async () => {
+    const url = "http://localhost:8000/api/properties/";
+
+    await fetch(url, {
+      method: "GET",
+    })
+      .then((Response) => Response.json())
+      .then((json) => {
+        console.log("json", json);
+        setPropertyList(json.data);
+      })
+      .catch((error) => {
+        console.log("err", error);
+      });
+  };
+
+  useEffect(() => {
+    getPropertyList();
+  }, []);
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {propertyList.map((property) => {
+        return <PropertyListItem key={property.id} property={property} />;
+      })}
     </>
   );
 };
--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
 
-const PropertyListItem = () => {
+import { PropertyType } from "./PropertyList";
+
+interface PropertyListItemProps {
+  property: PropertyType;
+}
+
+const PropertyListItem = ({ property }: PropertyListItemProps) => {
   return (
     <div className="cursor-pointer">
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           fill
-          src="/cabin_1.webp"
+          src={property.image_url}
           sizes="(max-width: 768px): 768px, (max-width: 1200px): 768px, 768px"
           className="hover:scale-110 object-cover transition h-full w-full"
           alt=""
         />
       </div>
       <div className="mt-2">
-        <p className="text-lg font-bold">Cabin</p>
+        <p className="text-lg font-bold">{property.title}</p>
       </div>
 
       <div className="mt-2">
         <p className="text-sm">
-          <strong>$200 CAD</strong> night
+          <strong>{property.price_per_night}</strong> night
         </p>
       </div>
     </div>
